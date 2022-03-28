4ab2e62f332067156b52ac20763670091d4ff446
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-27 22:44:39 -0400

    Initial commit

---
+++ src/main/java/com/cydeo/employeeregistrationprojectbyarpatjd2/EmployeeRegistrationProjectByArpatJd2Application.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregistrationprojectbyarpatjd2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeRegistrationProjectByArpatJd2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeRegistrationProjectByArpatJd2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/cydeo/employeeregistrationprojectbyarpatjd2/EmployeeRegistrationProjectByArpatJd2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cydeo.employeeregistrationprojectbyarpatjd2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeRegistrationProjectByArpatJd2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

5503e1df342235b61092342ecdcf9c6f628ac34d
Arafat9329 <68973655+Arafat9329@users.noreply.github.com>
2022-03-27 23:48:36 -0400

    employee-list.html page setUp

--- src/main/java/com/cydeo/controller/EmployeeController.java
+++ src/main/java/com/cydeo/controller/EmployeeController.java
@@ -1,9 +1,11 @@
 package com.cydeo.controller;
 
+import com.cydeo.bootstrap.DataGenerator;
 import com.cydeo.model.Employee;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -14,8 +16,15 @@ public class EmployeeController {
     public String createEmployees(Model model){
 
         model.addAttribute("employee", new Employee());
+        model.addAttribute("states", DataGenerator.getAllStates());
 
         return "employee/employee-create";
+    }
+
+    @PostMapping("/list")
+    public String employeeList(){
+
+        return "employee/employee-list";
     }
 
 }
